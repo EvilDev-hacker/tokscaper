72a88f5d4041cf79d6a403ae4fae7213f8c66f5c
EvilDev-hacker <websnew320@gmail.com>
2026-01-23 02:07:35 +0500

    new security futures added

--- server.js
+++ server.js
@@ -8,9 +8,28 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+
+const allowedOrigins = [
+  'http://tokisocialhub.ct.ws', 
+  'http://newkhnatok.ct.ws',
+  'http://tokliveshop.ct.ws',
+  'http://tokervipsocial.kesug.com',
+  'http://tookershop.ct.ws',
+  'localhost:3000'
+];
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
-app.use(cors())
+app.use(cors({
+  origin: function (origin, callback) {
+    // Agar origin list mein he ya phir request server-side se he (!origin)
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS Please Contact Developer: whatsapp: +447958266774'));
+    }
+  }
+}));
 
 app.get('/', (req, res) => { 
   res.sendFile(path.join(__dirname, 'index.html')); 
@@ -135,8 +154,26 @@ async function getTikTokData(username) {
   }
 }
 
+const protectAPI = (req, res, next) => {
+  // Browser batata he ke request kis site se aa rahi he (Origin)
+  const origin = req.get('origin') || req.get('referer');
+
+  if (!origin) {
+    return res.status(403).json({ error: "Direct access Denied. Please Contact Developer: whatsapp: +447958266774" });
+  }
+
+  // Check karna ke kya request karne wali site hamari list mein he?
+  const isAllowed = allowedOrigins.some(domain => origin.includes(domain));
+
+  if (isAllowed) {
+    next(); // Agar domain list mein he to scraper chalne do
+  } else {
+    return res.status(403).json({ error: "Access Denied: Domine Not Registered, Please Contact Developer: whatsapp: +447958266774" });
+  }
+};
+
 // Express endpoints
-app.get('/api/tiktok/:username', async (req, res) => {
+app.get('/api/tiktok/:username', protectAPI, async (req, res) => {
   try {
     const data = await getTikTokData(req.params.username);
 
